75623f9d5aef679250eacd7817b9534798f84ec6
literallyNotFate <dxfinitelynotfate@gmail.com>
2024-01-15 17:33:11 +0300

    1 site scraping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/scrape')
+  async scrape(@Query('url') url: string): Promise<any> {
+    if (!url) {
+      throw new Error('URL was not specified');
+    }
+
+    return this.appService.scrape(url);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async scrape(url: string): Promise<any> {
+    try {
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+
+      const result = [];
+      const names = $('.title > a')
+        .map((index, data) => $(data).text().replaceAll('\n', ''))
+        .get();
+
+      const specification = $('.specification')
+        .map((index, data) => $(data).text().trim())
+        .get();
+
+      const newPrice = $('.price-new')
+        .map((index, data) => $(data).text().trim())
+        .get();
+
+      const discount = $('.difprice')
+        .map((index, data) => $(data).text().trim())
+        .get();
+
+      for (let i = 0; i < names.length; i++) {
+        const data = {
+          name: names[i],
+          specification: specification[i],
+          newPrice: newPrice[i],
+          discount: discount[i],
+        };
+
+        result.push(data);
+      }
+
+      return result;
+    } catch (error) {
+      throw new Error(`Web-scraping error: ${error.message}`);
+    }
   }
 }
